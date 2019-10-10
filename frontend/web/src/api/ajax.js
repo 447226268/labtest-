4056ad1e05492b8692277bc8ae16d84a823eaf25
王勇 <wangyong@gooalgene.com>
2019-10-10 10:54:56 +0800

    ajax

--- frontend/web/src/api/ajax.js
+++ frontend/web/src/api/ajax.js
@@ -3,7 +3,7 @@
 import axios from 'axios'
 
 const service = axios.create({
-  baseURL: process.env.VUE_APP_BASE_API,//默认添加baseurl
+  baseURL: process.env.VUE_APP_BASE_API, //默认添加baseurl
 })
 
 
@@ -20,19 +20,36 @@ service.interceptors.request.use(
 //响应后需要处理的参数  -- 比如：判断token失效
 service.interceptors.response.use(
   response => {
-    return response;
+    if (response.status === 200) {
+      return Promise.resolve(response);
+    } else {
+      return Promise.reject(response);
+    }
   },
   error => {
-    return Promise.reject(error);
+    return Promise.reject(response);
   }
 );
 
 //外部使用axios
-export default function ajax(url, data={}, type='GET') {
-  return service({
-    url,
-    method:type,
-    data
-  })
+export default function ajax(url, data = {}, type = 'get') {
+  if((type).toLocaleLowerCase() === "get"){
+    return service({
+      url,
+      method: type,
+      params:data
+    })
+  }else if((type).toLocaleLowerCase() === "post"){
+    return service({
+      url,
+      method: type,
+      data
+    })
+  }else{
+    return service({
+      url,
+      method: type
+    })
+  }
+  
 }
-
