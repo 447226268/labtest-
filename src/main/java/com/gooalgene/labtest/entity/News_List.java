c8a0aad11b3702fe6e6b69da778806d2d9cfff2b
wangpf <937483278@qq.com>
2019-10-17 14:50:01 +0800

    10-17通知公告-wpf

--- src/main/java/com/gooalgene/labtest/entity/News_List.java
+++ src/main/java/com/gooalgene/labtest/entity/News_List.java
@@ -3,6 +3,8 @@ package com.gooalgene.labtest.entity;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Date;
+
 @Getter
 @Setter
 public class News_List {
@@ -15,9 +17,10 @@ public class News_List {
     private Integer nl_subType_id;
     private String nl_nl_subType_name;
     private Integer nl_state;
+    private Date nl_date;
     private String nl_url;
 
-    public News_List(Integer nl_id, String nl_title, String nl_content, String nl_graph, Integer nl_type_id, String nl_nl_type_name, Integer nl_subType_id, String nl_nl_subType_name, Integer nl_state, String nl_url) {
+    public News_List(Integer nl_id, String nl_title, String nl_content, String nl_graph, Integer nl_type_id, String nl_nl_type_name, Integer nl_subType_id, String nl_nl_subType_name, Integer nl_state, Date nl_date, String nl_url) {
         this.nl_id = nl_id;
         this.nl_title = nl_title;
         this.nl_content = nl_content;
@@ -27,21 +30,11 @@ public class News_List {
         this.nl_subType_id = nl_subType_id;
         this.nl_nl_subType_name = nl_nl_subType_name;
         this.nl_state = nl_state;
+        this.nl_date = nl_date;
         this.nl_url = nl_url;
     }
 
     public News_List() {
-    }
-
-    public News_List(Integer nl_id, String nl_title, String nl_content, String nl_graph, Integer nl_type_id, Integer nl_subType_id, Integer nl_state, String nl_url) {
-        this.nl_id = nl_id;
-        this.nl_title = nl_title;
-        this.nl_content = nl_content;
-        this.nl_graph = nl_graph;
-        this.nl_type_id = nl_type_id;
-        this.nl_subType_id = nl_subType_id;
-        this.nl_state = nl_state;
-        this.nl_url = nl_url;
     }
 
     @Override
@@ -56,6 +49,7 @@ public class News_List {
                 ", nl_subType_id=" + nl_subType_id +
                 ", nl_nl_subType_name='" + nl_nl_subType_name + '\'' +
                 ", nl_state=" + nl_state +
+                ", nl_date=" + nl_date +
                 ", nl_url='" + nl_url + '\'' +
                 '}';
     }
--- src/main/java/com/gooalgene/labtest/service/impl/NoticeService.java
+++ src/main/java/com/gooalgene/labtest/service/impl/NoticeService.java
@@ -1,17 +1,30 @@
 package com.gooalgene.labtest.service.impl;
 
+import com.gooalgene.labtest.dao.NoticeMapper;
 import com.gooalgene.labtest.entity.News_List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class NoticeService implements com.gooalgene.labtest.service.NoticeService {
+    @Autowired
+    NoticeMapper noticeMapper;
     @Override
     public List<News_List> findNotice() {
-        return null;
+        return noticeMapper.findNotice();
     }
 
     @Override
     public void addNotice(String[] title, int[] date) {
+        /*News_List notice = new News_List();
+        notice.setNl_id();
+        notice.setNl_title(title);
+        notice.setNl_content(content);
+        notice.setN*/
+
+
 
     }
 
