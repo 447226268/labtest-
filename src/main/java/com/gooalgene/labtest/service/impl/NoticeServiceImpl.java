87fd12374374daee867c4acaa8c9149b4fe66c2b
yxr <769977263@qq.com>
2019-10-17 15:11:46 +0800

    查询所有的公告

--- src/main/java/com/gooalgene/labtest/service/impl/NoticeServiceImpl.java
+++ src/main/java/com/gooalgene/labtest/service/impl/NoticeServiceImpl.java
@@ -1,5 +1,7 @@
 package com.gooalgene.labtest.service.impl;
 
+import com.gooalgene.labtest.dao.NewsSubTypeMapper;
+import com.gooalgene.labtest.dao.NewsTypeMapper;
 import com.gooalgene.labtest.dao.NoticeMapper;
 import com.gooalgene.labtest.entity.News_List;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,14 +10,23 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class NoticeService implements com.gooalgene.labtest.service.NoticeService {
+public class NoticeServiceImpl implements com.gooalgene.labtest.service.NoticeService {
     @Autowired
     NoticeMapper noticeMapper;
+    @Autowired
+    private NewsTypeMapper newsTypeMapper;
+    @Autowired
+    private NewsSubTypeMapper newsSubTypeMapper;
     @Override
     public List<News_List> findNotice() {
-        return noticeMapper.findNotice();
+        List<News_List> list = noticeMapper.findNotice();
+        for (News_List n : list) {
+            n.setNl_nl_type_name(
+                    newsTypeMapper.findById(n.getNl_type_id()).getNt_name());
+            n.setNl_nl_subType_name(newsSubTypeMapper.findById(n.getNl_subType_id()).getNs_name());
+        }
+        return list;
     }
-
     @Override
     public void addNotice(String[] title, int[] date) {
         /*News_List notice = new News_List();
