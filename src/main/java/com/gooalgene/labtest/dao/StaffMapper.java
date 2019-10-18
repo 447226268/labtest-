9112ac1c966fff7f5da9d8c08aac6a3bdad433e7
hys <1350460443@qq.com>
2019-10-18 11:31:24 +0800

    进一步完善staff

--- src/main/java/com/gooalgene/labtest/dao/StaffMapper.java
+++ src/main/java/com/gooalgene/labtest/dao/StaffMapper.java
@@ -1,5 +1,6 @@
 package com.gooalgene.labtest.dao;
 
+import com.gooalgene.labtest.entity.Staff_info;
 import com.gooalgene.labtest.entity.Staff_list;
 import org.springframework.stereotype.Repository;
 
@@ -7,6 +8,8 @@ import java.util.List;
 
 @Repository
 public interface StaffMapper {
+
+    //  Staff_list表
     List<Staff_list> findAll();
 
     Staff_list Sel(int id);
@@ -17,11 +20,19 @@ public interface StaffMapper {
 
     void Update(Staff_list staff);
 
+    //  Staff_type表
     String SearchType(int id);
 
     List<String> getAllType();
 
     Integer TypeToId(String st_name);
+
+    //  Staff_info表
+    Staff_info getStaffInfo(int uid);
+
+    void insertStaffInfo(Staff_info staff);
+
+    void deleteStaffInfo(int uid);
 }
 
 
---
+++ src/main/java/com/gooalgene/labtest/entity/Staff_info.java
@@ -0,0 +1,16 @@
+package com.gooalgene.labtest.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Staff_info {
+    private int si_id;
+    private int si_uid;
+    private String si_graph;
+    private String si_content;
+    private int si_state;
+    private String si_url;
+
+}
