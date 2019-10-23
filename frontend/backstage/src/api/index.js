51326bec2073afabf6c6d964f5435c55ec5f054b
zhouxin <zx15027188621@163.com>
2019-10-23 18:53:35 +0800

    后台前端0.1

--- frontend/backstage/src/api/index.js
+++ frontend/backstage/src/api/index.js
@@ -8,4 +8,26 @@ import ajax from './ajax';
  * @param type  请求方式
  */
 //Home
-export const test = () => ajax('/', {}) //测试
+export const test = () => ajax("/", {}) //测试
+
+
+
+//通知公告
+//获取所有通知显示在浏览页面上
+export const getNoticeall = () => ajax("/notice/findAll", {}) 
+//获取通知细节
+export const url_getNoticetIndex = "/notice/findById/"
+export const getNoticetIndex = (url_getNoticetIndex, data) => ajax(url_getNoticetIndex, data) 
+//更新通知
+export const url_updataNoticeIndex = "/notice/updateNotice/"
+export const updataNoticeIndex = (url_updataNoticeIndex, data, type) => ajax(url_updataNoticeIndex, data, "post") 
+//插入通知
+export const url_insertNotice = "/notice/insertNotice/"
+export const insertNotice = (url_insertNotice, data, type) => ajax(url_insertNotice, data, "post") 
+//删除通知
+export const url_deleteNotice = "/notice/deleteById/"
+export const deleteNotice = (url_deleteNotice, data, type) => ajax(url_deleteNotice, data, "get") 
+
+
+export const testhello = () => ajax("http://192.168.1.127:8083/haha/hello", {}) //通知
+export const testhello1 = (url, data,type) => ajax(url, data,type) //通知
---
+++ frontend/backstage/src/components/Uploadingmap.vue
@@ -0,0 +1,76 @@
+<template >
+  <div class="out-boder">
+    <div class="image-left">
+      <div class="image-text">新闻动态配图</div>
+      <el-upload
+        class="upload-demo"
+        drag
+        action="https://jsonplaceholder.typicode.com/posts/"
+      >
+        <i class="el-icon-upload"></i>
+        <div class="el-upload__text">
+          将文件拖到此处，或
+          <em>点击上传</em>
+        </div>
+        <div class="el-upload__tip" slot="tip">只能上传jpg/png文件，且不超过500kb</div>
+      </el-upload>
+    </div>
+
+    <div class="image-right">
+      <div class="image-text">学术活动配图</div>
+      <el-upload
+        class="upload-demo"
+        drag
+        action="https://jsonplaceholder.typicode.com/posts/"
+        multiple
+      >
+        <i class="el-icon-upload"></i>
+        <div class="el-upload__text">
+          将文件拖到此处，或
+          <em>点击上传</em>
+        </div>
+        <div class="el-upload__tip" slot="tip">只能上传jpg/png文件，且不超过500kb</div>
+      </el-upload>
+    </div>
+  </div>
+</template>
+
+<style>
+.out-boder {
+  border: 1px solid #c0c4cc;
+  height: 300px;
+}
+.image-left {
+  float: left;
+  margin-left: 10%;
+}
+.image-right {
+  float: left;
+  margin-left: 18%;
+}
+.image-text {
+  padding-top: 10px;
+  padding-bottom: 10px;
+  padding-left: 27px;
+}
+</style>
+
+<script>
+export default {
+  data() {
+    return {
+      dialogImageUrl: "",
+      dialogVisible: false
+    };
+  },
+  methods: {
+    handleRemove(file, fileList) {
+      console.log(file, fileList);
+    },
+    handlePictureCardPreview(file) {
+      this.dialogImageUrl = file.url;
+      this.dialogVisible = true;
+    }
+  }
+};
+</script>
