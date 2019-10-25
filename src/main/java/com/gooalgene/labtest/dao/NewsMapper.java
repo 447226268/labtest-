42d2780bf4425705fa4e076fb6588bd776a7a740
yxr <769977263@qq.com>
2019-10-25 22:37:08 +0800

    图片模块

---
+++ src/main/java/com/gooalgene/labtest/dao/NewsMapper.java
@@ -0,0 +1,28 @@
+package com.gooalgene.labtest.dao;
+
+import com.gooalgene.labtest.entity.News_List;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface NewsMapper {
+
+    News_List findById(@Param("nl_id") Integer nl_id);
+
+
+    List<News_List> findNews();
+
+    void deleteNews(@Param("nl_id") Integer noticeIds);
+
+    void addNews(News_List news_list);
+
+    void updateState(News_List news_list);
+
+    void updateNews(News_List news_list);
+
+}
+
+
+
