2db0f65abb3547215347c8ab41cbe787216f3c9a
yxr <769977263@qq.com>
2019-10-25 22:30:02 +0800

    新闻类型dao

---
+++ src/main/java/com/gooalgene/labtest/service/NewsService.java
@@ -0,0 +1,18 @@
+package com.gooalgene.labtest.service;
+
+import com.gooalgene.labtest.entity.News_List;
+
+import java.util.List;
+
+public interface NewsService {
+    List<News_List> findNews();
+
+    News_List findById(Integer nl_id);
+
+    void insertNews(News_List news_list);
+
+
+    void deleteNews(Integer noticeIds);
+
+    void updateNews(News_List news_list);
+}
