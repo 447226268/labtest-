9be076e87fe9fa253dc73732d756fb4b6b9905c2
zhouxin <zx15027188621@163.com>
2019-10-28 02:03:25 +0800

    论文总览接口显示数据，实验室简介，研究方向的页面

--- frontend/backstage/src/api/index.js
+++ frontend/backstage/src/api/index.js
@@ -14,41 +14,51 @@ export const test = () => ajax("/", {}) //测试
 //图片插入
 export const url_uploadPic = process.env.VUE_APP_BASE_API + "/news/uploadPic"
 
-
 //新闻发布
-//获取所有新闻显示在浏览页面上
+//获取所有显示在浏览页面上
 export const getNewsall = () => ajax("/news/findAll", {}) 
-//获取新闻细节
+//获取细节
 export const url_getNewsIndex = "/news/findById/"
 export const getNewsIndex = (url_getNewsIndex, data) => ajax(url_getNewsIndex, data) 
-//更新通知
+//更新
 export const url_updataNewsIndex = "/news/updateNews/"
 export const updataNewsIndex = (url_updataNewsIndex, data, type) => ajax(url_updataNewsIndex, data, "post") 
-//插入通知
+//插入
 export const url_insertNews = "/news/insertNews/"
 export const insertNews = (url_insertNews, data, type) => ajax(url_insertNews, data, "post") 
-// //删除通知
-// export const url_deleteNotice = "/notice/deleteById/"
-// export const deleteNotice = (url_deleteNotice, data, type) => ajax(url_deleteNotice, data, "get") 
-
-
+//删除
+export const url_deleteNews = "/news/deleteById/"
+export const deleteNews = (url_deleteNews, data, type) => ajax(url_deleteNews, data, "get") 
 
 //通知公告
-//获取所有通知显示在浏览页面上
+//获取所有显示在浏览页面上
 export const getNoticeall = () => ajax("/notice/findAll", {}) 
-//获取通知细节
+//获取细节
 export const url_getNoticetIndex = "/notice/findById/"
 export const getNoticetIndex = (url_getNoticetIndex, data) => ajax(url_getNoticetIndex, data) 
-//更新通知
+//更新
 export const url_updataNoticeIndex = "/notice/updateNotice/"
 export const updataNoticeIndex = (url_updataNoticeIndex, data, type) => ajax(url_updataNoticeIndex, data, "post") 
-//插入通知
+//插入
 export const url_insertNotice = "/notice/insertNotice/"
 export const insertNotice = (url_insertNotice, data, type) => ajax(url_insertNotice, data, "post") 
-//删除通知
+//删除
 export const url_deleteNotice = "/notice/deleteById/"
 export const deleteNotice = (url_deleteNotice, data, type) => ajax(url_deleteNotice, data, "get") 
 
+//科研工作——论文
+//获取所有显示在浏览页面上
+export const getArticleall = () => ajax("/thesis/findAll", {}) 
+//获取细节
+export const url_getArticleIndex = "/thesis/findById/"
+export const getArticleIndex = (url_getArticleIndex, data) => ajax(url_getArticleIndex, data) 
+//更新
+export const url_updataArticleIndex = "/thesis/updateThesis/"
+export const updataArticleIndex = (url_updataArticleIndex, data, type) => ajax(url_updataArticleIndex, data, "post") 
+//插入
+export const url_insertArticle = "/thesis/insertThesis/"
+export const insertArticle = (url_insertArticle, data, type) => ajax(url_insertArticle, data, "post") 
+//删除
+export const url_deleteArticle = "/thesis/deleteById/"
+export const deleteArticle = (url_deleteArticle, data, type) => ajax(url_deleteArticle, data, "get") 
 
-export const testhello = () => ajax("http://192.168.1.127:8083/haha/hello", {}) //通知
-export const testhello1 = (url, data,type) => ajax(url, data,type) //通知
