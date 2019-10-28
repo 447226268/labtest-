f4e2e680bab182955b97fc92f9d2daae9a030f41
胡宇松 <1350460443@qq.com>
2019-10-28 15:31:34 +0800

    bug修复

--- src/main/java/com/gooalgene/labtest/dao/ThesisMapper.java
+++ src/main/java/com/gooalgene/labtest/dao/ThesisMapper.java
@@ -11,18 +11,8 @@ public interface ThesisMapper {
 
 
     Thesis_List findById(@Param("tl_id") Integer nl_id);
-
-    List<Thesis_List> findThesis();
-
     void deleteThesis(@Param("tl_id") Integer noticeIds);
-
-    Thesis_List findById(@Param("tl_id") Integer tl_id);
-
     List<Thesis_List> findThesis();
-
-    void deleteThesis(@Param("tl_id") Integer thesisIds);
-
-
     void addThesis(Thesis_List thesis_list);
 
     void updateState(Thesis_List thesis_list);
